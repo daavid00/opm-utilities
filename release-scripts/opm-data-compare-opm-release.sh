303d3d11e587a0cc278646be255b490c8fced479
David Landa Marban <dmar@norceresearch.no>
2026-05-19 12:43:12 +0200

    Formating

--- release-scripts/opm-data-compare-opm-release.sh
+++ release-scripts/opm-data-compare-opm-release.sh
@@ -8,7 +8,7 @@
 # summary vectors (fopr fgpr fopt fgpt fwpt fpr). Takes as arguments the path to the first
 # flow executable (default "flow"), the second path (default "/path/to/coming/release"),
 # number of cores to run OPM Flow (default "8"), and deck names from opm-data to
-# run.
+# run. For macOS, this requires to install bash (e.g., brew install bash).
 #
 # Example: Test the comming release located in /Users/build/opm-simulators/bin/flow
 # with the current release using 32 CPUs for SPE10_MODEL2 and SLEIPNER_ORG.
@@ -23,15 +23,15 @@ FLOW2=${2:-/path/to/coming/release}
 CPUS=${3:-8}
 
 if [ ! -d venv ]; then
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install --upgrade pip setuptools wheel
-    pip install git+https://github.com/cssr-tools/plopm.git
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install --upgrade pip setuptools wheel
+  pip install git+https://github.com/cssr-tools/plopm.git
 else
-    source venv/bin/activate
+  source venv/bin/activate
 fi
 if ! command -v plopm >/dev/null 2>&1; then
-    pip install git+https://github.com/cssr-tools/plopm.git
+  pip install git+https://github.com/cssr-tools/plopm.git
 fi
 
 declare -A flows versions view slide projection cases
@@ -65,49 +65,48 @@ for arg in "${@:4}"; do
 done
 
 if [ ! -d "opm-data" ]; then
-    git clone https://github.com/OPM/opm-data.git
-    gunzip opm-data/sleipner/Sleipner_Reference_Model_cleaned.grdecl.gz
+  git clone https://github.com/OPM/opm-data.git
+  gunzip opm-data/sleipner/Sleipner_Reference_Model_cleaned.grdecl.gz
 fi
 
 for ((x = 1; x <= m; x++)); do
-    case=${cases[$x]}
-    if [ -d $case ]; then
-        rm -rf $case
+  case=${cases[$x]}
+  if [ -d $case ]; then
+    rm -rf $case
+  fi
+  mkdir $case
+  deck="$(find "$PWD" -name $case.DATA)"
+  # Add KEYWORDS to the SUMMARY
+  if ! grep -q "PERFORMA" $deck; then
+    sed -i.bak 's/^SUMMARY$/SUMMARY\
+    PERFORMA\nFOPR\nFGPR\nFOPT\nFGPT\nFWPT\nFPR/' $deck
+    sed -i.bak "s|SUMMARY |SUMMARY\nPERFORMA\nFOPR\nFGPR\nFOPT\nFGPT\nFWPT\nFPR |g" $deck
+    rm -f $deck.bak
+  fi
+  n=1
+  for flow in flow1 flow2; do
+    if grep -q ":" <<< ${flows[$flow]}; then
+      mkdir -p $case/FLOW$n
+      DECK_DIR=$(dirname ${deck})
+      DECK=$(basename ${deck})
+      docker run -v ${PWD}/$case/FLOW$n:/shared_host -v ${DECK_DIR}:/deck ${flows[$flow]} mpirun -np $CPUS flow /deck/$DECK --output-dir=/shared_host --parsing-strictness=low --check-satfunc-consistency=0
+    else
+      mpirun -np $CPUS ${flows[$flow]} $deck --output-dir=$case/FLOW$n --parsing-strictness=low --check-satfunc-consistency=0
     fi
-    mkdir $case
-    deck="$(find "$PWD" -name $case.DATA)"
-    # Add KEYWORDS to the SUMMARY
-    if ! grep -q "PERFORMA" $deck; then
-        sed -i.bak 's/^SUMMARY$/SUMMARY\
-        PERFORMA\nFOPR\nFGPR\nFOPT\nFGPT\nFWPT\nFPR/' $deck
-        sed -i.bak "s|SUMMARY |SUMMARY\nPERFORMA\nFOPR\nFGPR\nFOPT\nFGPT\nFWPT\nFPR |g" $deck
-        rm -f $deck.bak
-    fi
-    n=1
-    for flow in flow1 flow2; do
-        if grep -q ":" <<< ${flows[$flow]}
-        then
-          mkdir -p $case/FLOW$n
-          DECK_DIR=$(dirname ${deck})
-          DECK=$(basename ${deck})
-          docker run -v ${PWD}/$case/FLOW$n:/shared_host -v ${DECK_DIR}:/deck ${flows[$flow]} mpirun -np $CPUS flow /deck/$DECK --output-dir=/shared_host --parsing-strictness=low --check-satfunc-consistency=0
-        else
-          mpirun -np $CPUS ${flows[$flow]} $deck --output-dir=$case/FLOW$n --parsing-strictness=low --check-satfunc-consistency=0
-        fi
-        n=$((n + 1))
-    done
-    cd $case
-    # Spatial maps for oil saturation (modify this for a different quantity, i.e., pressure)
-    for ((i = 1; i <= 3; i++)); do
-        plopm -i 'FLOW1/ FLOW2/' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v soil -clabel "${view[$i]} oil saturation (end of simulation)" -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s "${slide[$i]}" -save slide$i
-        plopm -i 'FLOW1/' -v soil -cnum 5 -d 10,8 -z 0 -diff 'FLOW2/' -s "${slide[$i]}" -t "${view[$i]} oil saturation (Flow ${versions[flow1]}-Flow ${versions[flow2]}, end of simulation)" -save diff_slide$i
-        plopm -i 'FLOW1/ FLOW2/' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v soil -clabel "${view[$i]} pore volume weighted oil saturation projection (end of simulation)" -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s "${projection[$i]}" -save projection$i
-        plopm -i 'FLOW1/' -v soil -cnum 5 -d 10,8 -z 0 -diff 'FLOW2/' -t "${view[$i]} pore volume weighted oil saturation projection (Flow ${versions[flow1]}-Flow ${versions[flow2]}, end of simulation)" -s "${projection[$i]}" -save diff_projection$i
-    done
-    for quantity in tcpu msumlins msumnewt fopr fgpr fopt fgpt fwpt fpr; do
-        plopm -i "FLOW1/ FLOW2/" -v $quantity -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits y -xformat .0f
-    done
-    cd ..
+    n=$((n + 1))
+  done
+  cd $case
+  # Spatial maps for oil saturation (modify this for a different quantity, i.e., pressure)
+  for ((i = 1; i <= 3; i++)); do
+    plopm -i 'FLOW1/ FLOW2/' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v soil -clabel "${view[$i]} oil saturation (end of simulation)" -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s "${slide[$i]}" -save slide$i
+    plopm -i 'FLOW1/' -v soil -cnum 5 -d 10,8 -z 0 -diff 'FLOW2/' -s "${slide[$i]}" -t "${view[$i]} oil saturation (Flow ${versions[flow1]}-Flow ${versions[flow2]}, end of simulation)" -save diff_slide$i
+    plopm -i 'FLOW1/ FLOW2/' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v soil -clabel "${view[$i]} pore volume weighted oil saturation projection (end of simulation)" -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s "${projection[$i]}" -save projection$i
+    plopm -i 'FLOW1/' -v soil -cnum 5 -d 10,8 -z 0 -diff 'FLOW2/' -t "${view[$i]} pore volume weighted oil saturation projection (Flow ${versions[flow1]}-Flow ${versions[flow2]}, end of simulation)" -s "${projection[$i]}" -save diff_projection$i
+  done
+  for quantity in tcpu msumlins msumnewt fopr fgpr fopt fgpt fwpt fpr; do
+    plopm -i "FLOW1/ FLOW2/" -v $quantity -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits y -xformat .0f
+  done
+  cd ..
 done
 
 deactivate
--- release-scripts/spe11-compare-opm-release.sh
+++ release-scripts/spe11-compare-opm-release.sh
@@ -7,7 +7,8 @@
 # middle part) and for performance quantities msumlins and msumnewt, in addition to
 # the total (theoretical) mass. Takes as arguments the path to the first flow
 # executable (default "flow"), the second path (default "/path/to/coming/release"),
-# and number of cores to run OPM Flow (default "8").
+# and number of cores to run OPM Flow (default "8"). For macOS, this requires to
+# install bash (e.g., brew install bash).
 #
 # Example: Test the comming release located in /Users/build/opm-simulators/bin/flow
 # with the current release using 8 CPUs.
@@ -22,26 +23,36 @@ FLOW2=${2:-/path/to/coming/release}
 CPUS=${3:-8}
 
 if [ ! -d venv ]; then
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install --upgrade pip setuptools wheel
-    pip install git+https://github.com/cssr-tools/plopm.git
-    pip install git+https://github.com/OPM/pyopmspe11.git
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install --upgrade pip setuptools wheel
+  pip install git+https://github.com/cssr-tools/plopm.git
+  pip install git+https://github.com/OPM/pyopmspe11.git
 else
-    source venv/bin/activate
+  source venv/bin/activate
 fi
 if ! command -v pyopmspe11 >/dev/null 2>&1; then
-    pip install git+https://github.com/OPM/pyopmspe11.git
+  pip install git+https://github.com/OPM/pyopmspe11.git
 fi
 if ! command -v plopm >/dev/null 2>&1; then
-    pip install git+https://github.com/cssr-tools/plopm.git
+  pip install git+https://github.com/cssr-tools/plopm.git
 fi
 
 declare -A flows versions total_mass units slide format cases mpiruns 
 flows[flow1]=$FLOW1
 flows[flow2]=$FLOW2
-versions[flow1]="$($FLOW1 --version | cut -c6-)"
-versions[flow2]="$($FLOW2 --version | cut -c6-)"
+if grep -q ":" <<< $FLOW1
+then
+  versions[flow1]="$(docker run $FLOW1 flow --version | cut -c6-)"
+else
+  versions[flow1]="$($FLOW1 --version | cut -c6-)"
+fi
+if grep -q ":" <<< $FLOW2
+then
+  versions[flow2]="docker run $FLOW2 flow --version | cut -c6-)"
+else
+  versions[flow2]="$($FLOW2 --version | cut -c6-)"
+fi
 cases[a]=r3_cp_1cmish_capmax2500Pa
 cases[b]=r2_cp_10mish
 cases[c]=r2_cp_50m-50m-8mish
@@ -62,27 +73,27 @@ format[msumlins]=.0f
 format[msumnewt]=.0f
 
 for x in a b c; do
-    if [ -d spe11$x ]; then
-        rm -rf spe11$x
-    fi
-    mkdir spe11$x && cd spe11$x
-    n=1
-    for flow in flow1 flow2; do
-        curl -L -O https://raw.githubusercontent.com/OPM/pyopmspe11/refs/heads/main/benchmark/spe11$x/${cases[$x]}.toml
-        sed -i.bak "s|-np ${mpiruns[$x]}|-np $CPUS|g" ${cases[$x]}.toml
-        sed -i.bak "s|flow --|${flows[$flow]} --|g" ${cases[$x]}.toml
-        rm -f ${cases[$x]}.toml.bak
-        pyopmspe11 -i ${cases[$x]}.toml -o FLOW$n -f 0
-        n=$((n + 1))
-    done
-    # Spatial maps for CO2 mass fraction (modify this for a different quantity, i.e., pressure)
-    plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v xco2l -clabel 'CO$_2$ mass fraction (liquid phase, end of simulation)' -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s ,${slide[$x]},
-    plopm -i 'FLOW1/FLOW1' -t "Flow ${versions[flow1]} - Flow ${versions[flow2]}, end of simulation" -v xco2l -clabel 'CO$_2$ mass fraction (liquid phase)' -cnum 5 -d 10,8 -z 0 -s ,${slide[$x]}, -diff FLOW2/FLOW2
-    for quantity in tcpu msumlins msumnewt
-    do  plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -v $quantity -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits ${units[$x]} -xformat .0f -yformat ${format[$quantity]}
-    done
-    plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -v fgmip -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits ${units[$x]} -xformat .0f -t "Total CO2 mass injected: ${total_mass[$x]} [kg]"
-    cd ..
+  if [ -d spe11$x ]; then
+    rm -rf spe11$x
+  fi
+  mkdir spe11$x && cd spe11$x
+  n=1
+  for flow in flow1 flow2; do
+    curl -L -O https://raw.githubusercontent.com/OPM/pyopmspe11/refs/heads/main/benchmark/spe11$x/${cases[$x]}.toml
+    sed -i.bak "s|-np ${mpiruns[$x]}|-np $CPUS|g" ${cases[$x]}.toml
+    sed -i.bak "s|flow --|${flows[$flow]} --|g" ${cases[$x]}.toml
+    rm -f ${cases[$x]}.toml.bak
+    pyopmspe11 -i ${cases[$x]}.toml -o FLOW$n -f 0
+    n=$((n + 1))
+  done
+  # Spatial maps for CO2 mass fraction (modify this for a different quantity, i.e., pressure)
+  plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -subfigs 2,1 -t "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -cbsfax 0.25,0.93,0.5,0.02 -delax 1 -v xco2l -clabel 'CO$_2$ mass fraction (liquid phase, end of simulation)' -suptitle 0 -cnum 5 -cformat .1e -d 10,8 -z 0 -s ,${slide[$x]},
+  plopm -i 'FLOW1/FLOW1' -t "Flow ${versions[flow1]} - Flow ${versions[flow2]}, end of simulation" -v xco2l -clabel 'CO$_2$ mass fraction (liquid phase)' -cnum 5 -d 10,8 -z 0 -s ,${slide[$x]}, -diff FLOW2/FLOW2
+  for quantity in tcpu msumlins msumnewt; do
+    plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -v $quantity -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits ${units[$x]} -xformat .0f -yformat ${format[$quantity]}
+  done
+  plopm -i 'FLOW1/FLOW1 FLOW2/FLOW2' -v fgmip -labels "Flow ${versions[flow1]}  Flow ${versions[flow2]}" -tunits ${units[$x]} -xformat .0f -t "Total CO2 mass injected: ${total_mass[$x]} [kg]"
+  cd ..
 done
 
 deactivate
